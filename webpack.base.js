ef54a53eadbc4576718e5d1a5917278f7458b1fc
jiangwei <jiangwei07061625@163.com>
2019-06-09 10:46:05 +0800

    test bundle split

--- webpack.base.js
+++ webpack.base.js
@@ -2,6 +2,7 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     module: {
