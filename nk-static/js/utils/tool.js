b439f239cbee2596a3ae07c98f0a8fc4e2aa10f4
jiangwei <jiangwei07061625@163.com>
2019-07-27 10:59:07 +0800

    optimised for util tool

--- nk-static/js/utils/tool.js
+++ nk-static/js/utils/tool.js
@@ -17,10 +17,10 @@ var Tool = {
      * @param {int} length 字符串长度
      * @return {string} 随机字符串
      */
-    'createNonceStr': function (length) {
-        var str = '';
-        var pos = 0;
-        for (var i = 0; i < length; i++) {
+    'createNonceStr': (length) => {
+        let str = '';
+        let pos = 0;
+        for (let i = 0; i < length; i++) {
             pos = Math.round(Math.random() * 31);
             str += this.chars[pos];
         }
@@ -31,8 +31,8 @@ var Tool = {
      * 获取浏览器类型
      * @return {string} 浏览器类型
      */
-    'getBrowserType': function () {
-        var userAgent = window.navigator.userAgent;
+    'getBrowserType': () => {
+        let userAgent = window.navigator.userAgent;
         if (/MicroMessenger/.test(userAgent)) {
             return 'wx';
         }
@@ -43,9 +43,9 @@ var Tool = {
             return 'opera';
         }
         if ((userAgent.indexOf('compatible') > -1) && (userAgent.indexOf('MSIE') > -1)) {
-            var reIE = new RegExp('MSIE (\\d+\\.\\d+);');
+            let reIE = new RegExp('MSIE (\\d+\\.\\d+);');
             reIE.test(userAgent);
-            var ieVersion = parseInt(RegExp.$1, 10);
+            let ieVersion = parseInt(RegExp.$1, 10);
             if (ieVersion >= 7) {
                 return 'ie' + ieVersion.toString();
             } else {
@@ -61,7 +61,7 @@ var Tool = {
         if (userAgent.indexOf('Firefox') > -1) {
             return 'firefox';
         }
-        var chromeIndex = userAgent.indexOf('Chrome');
+        let chromeIndex = userAgent.indexOf('Chrome');
         if ((userAgent.indexOf('Safari') > -1) && (chromeIndex < 0)) {
             return 'safari';
         }
@@ -76,27 +76,21 @@ var Tool = {
      * @param {string} str 字符串
      * @return {string} 字符串
      */
-    'ltrim': function (str) {
-        return str.replace(/^(\s|\u00A0)+/, '');
-    },
+    'ltrim': (str) => str.replace(/^(\s|\u00A0)+/, ''),
 
     /**
      * 去除字符串右边空格
      * @param {string} str 字符串
      * @return {string} 字符串
      */
-    'rtrim': function (str) {
-        return str.replace(/(\s|\u00A0)+$/, '');
-    },
+    'rtrim': (str) => str.replace(/(\s|\u00A0)+$/, ''),
 
     /**
      * 去除字符串前后空格
      * @param {string} str 字符串
      * @return {string} 字符串
      */
-    'trim': function (str) {
-        return str.replace(/^(\s|\u00A0)+/, '').replace(/(\s|\u00A0)+$/, '');
-    },
+    'trim': (str) => str.replace(/^(\s|\u00A0)+/, '').replace(/(\s|\u00A0)+$/, ''),
 
     /**
      * 日期格式化
@@ -104,9 +98,9 @@ var Tool = {
      * @param {string} format 格式字符串 yyyy-MM-dd hh:mm:ss.S
      * @return {string} 格式化后的日期
      */
-    'dateFormat': function (timestamp, format) {
-        var nowDate = new Date(timestamp);
-        var o = {
+    'dateFormat': (timestamp, format) => {
+        let nowDate = new Date(timestamp);
+        let o = {
             'M+': nowDate.getMonth() + 1,
             'd+': nowDate.getDate(),
             'h+': nowDate.getHours(),
@@ -115,7 +109,7 @@ var Tool = {
             'q+': Math.floor((nowDate.getMonth() + 3) / 3),
             'S': nowDate.getMilliseconds()
         };
-        var week = {
+        let week = {
             '0': '/u65e5',
             '1': '/u4e00',
             '2': '/u4e8c',
@@ -131,11 +125,100 @@ var Tool = {
         if (/(E+)/.test(format)) {
             format = format.replace(RegExp.$1, ((RegExp.$1.length > 1) ? (RegExp.$1.length > 2 ? '/u661f/u671f' : '/u5468') : '') + week[nowDate.getDay() + '']);
         }
-        for (var k in o) {
+        for (let k in o) {
             if (new RegExp('(' + k + ')').test(format)) {
                 format = format.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)));
             }
         }
         return format;
-    }
+    },
+
+    // ------代码优化开始------
+    // 参考链接: https://30secondsofcode.org/
+    /**
+     * 判断数组中所有元素是否都满足条件
+     * @param {array} arr 数组数据
+     * @param {function|bool} fn 判断条件
+     * @return {bool} 判断结果
+     */
+    'arrayAll': (arr, fn = true) => arr.every(fn),
+
+    /**
+     * 判断数组中所有元素相等
+     * @param {array} arr 数组数据
+     * @return {bool} 判断结果
+     */
+    'arrayAllEqual': (arr) => arr.every((val) => val === arr[0]),
+
+    /**
+     * 判断数组中是否存在元素满足条件
+     * @param {array} arr 数组数据
+     * @param {function|bool} fn 判断条件
+     * @return {bool} 判断结果
+     */
+    'arrayAny': (arr, fn = true) => arr.some(fn),
+
+    /**
+     * 数组转CSV
+     * @param {array} arr 数组数据
+     * @param {string} delimiter 分隔符
+     * @return {string} 转化结果
+     */
+    'array2CSV': (arr, delimiter = ',') => arr.map((v) => v.map((x) => (isNaN(x) ? `"${x.replace(/"/g, '""')}"` : x)).join(delimiter)).join('\n'),
+
+    /**
+     * 数据过滤
+     * @param {array} arr 数组数据
+     * @param {array} filter 过滤器
+     * @return {array} 过滤结果
+     */
+    'arrayBifurcate': (arr, filter) => arr.reduce((acc, val, i) => acc[filter[i] ? 0 : 1].push(val), [[], []]),
+
+    /**
+     * 根据条件过滤数据
+     * @param {array} arr 数组数据
+     * @param {function} fn 过滤条件
+     * @return {array} 过滤结果
+     */
+    'arrayBifurcateBy': (arr, fn) => arr.reduce((acc, val, i) => acc[fn(val, i) ? 0 : 1].push(val), [[], []]),
+
+    /**
+     * 数组分块
+     * @param {array} arr 数组数据
+     * @param {int} size 分块大小
+     * @return {array} 分块结果
+     */
+    'arrayChunk': (arr, size) => Array.from({length: Math.ceil(arr.length / size)}, (v, i) => arr.slice(i * size, i * size + size)),
+
+    /**
+     * 获取数组中指定元素的个数
+     * @param {array} arr 数组数据
+     * @param {obj} val 元素值
+     * @return {int} 元素个数
+     */
+    'arrayCountOccurrences': (arr, val) => arr.reduce((a, v) => (v === val ? a + 1 : a), 0),
+
+    /**
+     * 获取两个数组的不同元素
+     * @param {array} a 数组数据
+     * @param {array} b 数组数据
+     * @return {array} 获取结果
+     */
+    'arrayDifference': (a, b) => {
+        const s = new Set(b);
+        return a.filter((x) => !s.has(x));
+    },
+
+    /**
+     * 根据过滤器获取两个数组的不同元素
+     * @param {array} a 数组数据
+     * @param {array} b 数组数据
+     * @param {function} fn 过滤器
+     * @return {array} 获取结果
+     */
+    'arrayDifferenceBy': (a, b, fn) => {
+        const s = new Set(b.map(fn));
+        return a.map(fn).filter((el) => !s.has(el));
+    },
+    // ------代码优化结束------
 };
