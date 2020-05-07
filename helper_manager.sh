d5151997814c0971c8f17bbdd876968431d41add
jiangwei <jiangwei07061625@163.com>
2020-05-07 17:37:00 +0800

    fixed error for helper shell

--- helper_manager.sh
+++ helper_manager.sh
@@ -25,7 +25,7 @@ SUFFIX_JS=".js"
 PID=`ps -A -o pid,ppid,cmd|grep node$PROJECT_TAG|grep -v 'grep'|awk 'BEGIN{pid=0} {pid=$1} END{print pid}'`
 # 启动命令
 COMMAND_START="start --minUptime 10000 --spinSleepTime 30000 --pidFile $DIR_LOG/$PROJECT_TAG$SUFFIX_PID -l $DIR_LOG/forever_$PROJECT_TAG$SUFFIX_LOG -o $DIR_LOG/out_$PROJECT_TAG$SUFFIX_LOG -e $DIR_LOG/error_$PROJECT_TAG$SUFFIX_LOG -a -c nodemon $PROJECT_TAG$SUFFIX_JS --exitcrash"
-# js脚本文件后缀
+# js检测结果文件
 FILE_CHECK_RESULT="rule_check.log"
 
 # 刷新模块
