3691a88694d376054112f40a2d2008434c430975
jiangwei <jiangwei07061625@163.com>
2020-05-08 18:35:29 +0800

    add create package.json

--- helper_manager.sh
+++ helper_manager.sh
@@ -26,6 +26,56 @@ COMMAND_START="start --minUptime 10000 --spinSleepTime 30000 --pidFile $DIR_LOG/
 # js检测结果文件
 FILE_CHECK_RESULT="rule_check.log"
 
+# 生成package.json
+function createPackageJson() {
+    rm -rf package.json
+    touch package.json
+    echo "{" >> package.json
+    echo "  \"name\": \"$PACKAGE_NAME\"," >> package.json
+    echo "  \"version\": \"$PACKAGE_VERSION\"," >> package.json
+    echo "  \"description\": \"$PACKAGE_DESCRIPTION\"," >> package.json
+    echo "  \"main\": \"$PROJECT_TAG.js\"," >> package.json
+    echo "  \"scripts\": {" >> package.json
+    echo "    \"build-product\": \"webpack --config webpack.production.js -p\"" >> package.json
+    echo "  }," >> package.json
+    echo "  \"repository\": {" >> package.json
+    echo "    \"type\": \"$PACKAGE_REPOSITORY_TYPE\"," >> package.json
+    echo "    \"url\": \"$PACKAGE_REPOSITORY_URL\"" >> package.json
+    echo "  }," >> package.json
+    echo "  \"keywords\": [" >> package.json
+    echo "    \"front\"," >> package.json
+    echo "    \"ssr",\" >> package.json
+    echo "    \"web",\" >> package.json
+    echo "    \"framework\"," >> package.json
+    echo "    \"node\"," >> package.json
+    echo "    \"koa2\"," >> package.json
+    echo "    \"nunjucks\"" >> package.json
+    echo "  ]," >> package.json
+    echo "  \"author\": \"$PACKAGE_AUTHOR\"," >> package.json
+    echo "  \"contact\": \"$PACKAGE_CONTACT\"," >> package.json
+    echo "  \"license\": \"$PACKAGE_LICENSE\"," >> package.json
+    echo "  \"bugs\": {" >> package.json
+    echo "    \"url\": \"$PACKAGE_BUGS_URL\"" >> package.json
+    echo "  }," >> package.json
+    echo "  \"homepage\": \"$PACKAGE_HOMEPAGE\"," >> package.json
+    echo "  \"os\": [" >> package.json
+    echo "    \"darwin\"," >> package.json
+    echo "    \"linux\"" >> package.json
+    echo "  ]," >> package.json
+    echo "  \"engines\": {" >> package.json
+    echo "    \"node\": \"^10.15.3\"," >> package.json
+    echo "    \"nodemon\": \"^1.18.11\"," >> package.json
+    echo "    \"forever\": \"^1.0.0\"," >> package.json
+    echo "    \"eslint\": \"^5.16.0\"," >> package.json
+    echo "    \"webpack\": \"^4.43.0\"," >> package.json
+    echo "    \"webpack-cli\": \"^3.3.11\"" >> package.json
+    echo "  }," >> package.json
+    echo "  \"dependencies\": {" >> package.json
+    echo "    \"async\": \"^3.2.0\"" >> package.json
+    echo "  }" >> package.json
+    echo "}" >> package.json
+}
+
 # 刷新模块
 function refreshModule() {
     # 框架公共库更新
@@ -87,13 +137,14 @@ function formatJs() {
 case "$1" in
     init)
         rm -rf $DIR_ROOT/node_modules
-        npm init -y
-        # 框架所需模块
-        npm install koa@2 koa-router@7 nunjucks@3 html-minifier@4 lru-cache@5 utility axios lodash eventproxy async qs --save
-        # webpack打包所需模块
-        npm install webpack@4 webpack-cli@3 glob mini-css-extract-plugin style-loader css-loader sass-loader postcss-loader less-loader less autoprefixer uglifyjs-webpack-plugin optimize-css-assets-webpack-plugin cssnano clean-webpack-plugin purgecss-webpack-plugin purify-css html-withimg-loader file-loader url-loader mkdirp peer html-webpack-plugin@3 bundle-loader promise-loader ttf-loader --save
-        cnpm install node-sass --save
-        echo "init project success"
+        createPackageJson
+#        npm init -y
+#        # 框架所需模块
+#        npm install koa@2 koa-router@7 nunjucks@3 html-minifier@4 lru-cache@5 utility axios lodash eventproxy async qs --save
+#        # webpack打包所需模块
+#        npm install webpack@4 webpack-cli@3 glob mini-css-extract-plugin style-loader css-loader sass-loader postcss-loader less-loader less autoprefixer uglifyjs-webpack-plugin optimize-css-assets-webpack-plugin cssnano clean-webpack-plugin purgecss-webpack-plugin purify-css html-withimg-loader file-loader url-loader mkdirp peer html-webpack-plugin@3 bundle-loader promise-loader ttf-loader --save
+#        cnpm install node-sass --save
+#        echo "init project success"
         ;;
     start)
         if [ $PID -gt 0 ]; then
