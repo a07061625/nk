37946c7eb343a0a3464bc02c135a319e1c5eb78b
jiangwei <jiangwei07061625@163.com>
2020-05-08 23:50:38 +0800

    restruct shell script

---
+++ nk-static/env_frame.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# 框架公共库node模块目录
+DIR_LIB_FRAME_MODULE=$DIR_ROOT/node_modules/nk-frame
+# 项目公共库node模块目录
+DIR_LIB_PROJECT_MODULE=$DIR_ROOT/node_modules/nk-project
+# 项目公共库原始目录
+DIR_LIB_PROJECT_ORIGIN=$DIR_ROOT/nk-project
+# 进程ID文件后缀
+SUFFIX_PID=".pid"
+# 日志文件后缀
+SUFFIX_LOG=".log"
+# js脚本文件后缀
+SUFFIX_JS=".js"
+# 进程ID
+PID=`ps -A -o pid,ppid,cmd|grep node$PROJECT_TAG|grep -v 'grep'|awk 'BEGIN{pid=0} {pid=$1} END{print pid}'`
+# 启动命令
+COMMAND_START="start --minUptime 10000 --spinSleepTime 30000 --pidFile $DIR_LOG/$PROJECT_TAG$SUFFIX_PID -l $DIR_LOG/forever_$PROJECT_TAG$SUFFIX_LOG -o $DIR_LOG/out_$PROJECT_TAG$SUFFIX_LOG -e $DIR_LOG/error_$PROJECT_TAG$SUFFIX_LOG -a -c nodemon $PROJECT_TAG$SUFFIX_JS --exitcrash"
+# js检测结果文件
+FILE_CHECK_RESULT="rule_check.log"
---
+++ static/env_project_base.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 日志根目录
+DIR_LOG=/home/logs/nodeforever
+# 框架公共库原始目录
+DIR_LIB_FRAME_ORIGIN=/path/to/nklibs
+# 项目标识 nk+3位由数字字母组成的字符串
+PROJECT_TAG=nka01
+# package.json相关
+PACKAGE_NAME="nk"
+PACKAGE_VERSION="2.0.0"
+PACKAGE_DESCRIPTION="front ssr framework,base on nodejs v10.15.3+,koa v2.7.0+,nunjucks v3.2.0+"
+PACKAGE_REPOSITORY_TYPE="git"
+PACKAGE_REPOSITORY_URL="git+https://github.com/a07061625/nk.git"
+PACKAGE_AUTHOR="jiangwei"
+PACKAGE_CONTACT="QQ:837483732 Email:jiangwei07061625@163.com"
+PACKAGE_LICENSE="BSD-3-Clause"
+PACKAGE_BUGS_URL="https://github.com/a07061625/nk/issues"
+PACKAGE_HOMEPAGE="https://github.com/a07061625/nk#readme"
